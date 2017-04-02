1be61a0680eb5188e6e4eda1bf46ffbfafb1179a
rk <shiningciel@gmail.com>
2017-04-02 19:50:18 +0900

    add the sample source code

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <1.h>
+//#include <2.h>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//        int temp=0;
+//
+//                cout<<"temp is "<<temp<<endl;
+//
+//
+//                        return 0;
+//                        }
+//
